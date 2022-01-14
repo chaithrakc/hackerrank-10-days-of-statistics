62e19d1e70eff03e2c38835431af4408f5432f76
Chaithra K C <chaithra.kc22@gmail.com>
2022-01-14 14:54:52 -0500

    day0 program impl

--- day0/p01mean_median_mode.py
+++ day0/p01mean_median_mode.py
@@ -2,34 +2,29 @@ import math
 from collections import Counter
 
 
-# central measures - mean, median, and mode
+# central measures/central tendency of the data - mean, median, and mode
 
-def mean(nums: list) -> float:
-    return sum(nums) / len(nums)
-
-
-def median(nums: list) -> float:
-    size = len(nums)
-    nums_copy = nums.copy()
-    nums_copy.sort()
-    # even length
-    if size % 2 == 0:
-        median_index = size // 2
-        return (nums_copy[median_index - 1] + nums_copy[median_index]) / 2
-    # odd length
-    median_index = math.ceil(size / 2)
-    return nums_copy[median_index]
+def mean(sample: list) -> float:
+    return sum(sample) / len(sample)
 
 
-def mode(nums: list) -> int:
-    value_count = Counter(nums)
-    max_freq = value_count.most_common(1)[0][1]
-    most_common = list()
-    for item, value in value_count.items():
-        if value == max_freq:
-            most_common.append(item)
-    most_common.sort()
-    return most_common[0]
+def median(sample: list) -> float:
+    size = len(sample)
+    mid_index = size // 2
+    # sample with odd length
+    if size % 2:
+        return sorted(sample)[mid_index]
+
+    # sample with even length
+    return sum(sorted(sample)[mid_index - 1: mid_index + 1]) / 2
+
+
+def mode(sample: list) -> int:
+    ctr = Counter(sample)
+    max_freq = ctr.most_common(1)[0][1]
+    max_freq_num = [key for key, val in ctr.items() if val == max_freq]
+    # if multiple values have max freq, selecting the smallest one
+    return sorted(max_freq_num)[0]
 
 
 if __name__ == '__main__':
@@ -37,7 +32,7 @@ if __name__ == '__main__':
     # size = int(input())
     # data = list(map(int, input().split()))
 
-    data = [64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060]
+    data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     average = mean(data)
     median_val = median(data)
     mode_val = mode(data)
--- day0/p02weighted_mean.py
+++ day0/p02weighted_mean.py
@@ -1,14 +1,21 @@
 # weighted mean tutorial - real time example
 # https://www.hackerrank.com/challenges/s10-weighted-mean/tutorial
 
+import numpy as np
+
+
 def weighted_mean(values: list, costs: list) -> float:
-    total_weight = 0
-    for val, cost in zip(values, costs):
-        total_weight = total_weight + (val * cost)
-    return '%.1f' % (total_weight / sum(costs))
+    total_weight = sum([val * cost for val, cost in zip(values, costs)])
+    weighted_avg = total_weight / sum(costs)
+    return '%.1f' % weighted_avg
 
 
 if __name__ == '__main__':
     data = [10, 40, 30, 50, 20]
     weights = [1, 2, 3, 4, 5]
     print(weighted_mean(data, weights))
+
+    # using numpy
+    avg = np.average(np.array(data), weights=np.array(weights))
+    print(avg)
+
